b2a25557f624c56ab1ebb47fcc0b623de8e0c08b
kashyap_anjali <133110531+kashyapanjali@users.noreply.github.com>
2025-11-16 13:17:08 +0530

    user router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import "./App.css";
 
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+
 function App() {
-	return <div className='App'></div>;
+	return (
+		<div className='App'>
+			<Router>
+				<Navbar />
+				<Routes>
+					<Route
+						path='/'
+						element={<Home />}
+					/>
+					<Route
+						path='/about'
+						element={<About />}
+					/>
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function About() {
+	return (
+		<div>
+			<h2>
+				This is about iNotebook app. This app is developed by Anjali Kashyap to
+				take notes easily and efficiently.
+			</h2>
+		</div>
+	);
+}
+
+export default About;
